900bf69d4e69ff316d5a735f9ea1aaed87cd0b38
Jeff Taggart <jeff@jetaggart.com>
2014-09-24 10:10:48 -0600

    Unpend body spec

--- spec/parse_response_spec.rb
+++ spec/parse_response_spec.rb
@@ -23,7 +23,6 @@ describe 'HTTPResponseParser' do
     end
 
     it 'should return the body' do
-      skip
       expect(parse_response('lib/response_200.txt').body).to eq("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\" />  <meta name=\"description\" content=\"should i test private methods?\" />  <meta name=\"keywords\" content=\"test,private,methods,oo,object,oriented,tdd\" />  <title>Should I Test Private Methods?</title></head><body>  <div style='font-size: 96px; font-weight: bold; text-align: center; padding-top: 200px; font-family: Verdana, Helvetica, sans-serif'>NO</div>  <!-- Every time you consider testing a private method, your code is telling you that you haven't allocated responsibilities well.  Are you listening to it? --></body></html>")
     end
 
